76308b18932d076731e16bc28120f2f49c86ba56
unknown <rafael.soares0777@gmail.com>
2022-02-16 22:14:49 -0300

    Final da aula

---
+++ app.js
@@ -0,0 +1,32 @@
+//Comentário de linha
+
+/* 
+Comentário de bloco
+*/
+
+const XPTO = 10;
+
+console.log(XPTO);
+
+let minhaVar = 'Olá';
+
+minhaVar = minhaVar + ' Mundo!';
+
+console.log(minhaVar);
+
+function condLoop()
+{
+    if (true)
+        console.log('IF verdadeiro!');
+    else
+        console.log('IF falso!');
+
+    for (let i = 0; i < 10; i++)
+        console.log('Repetição ' + i);
+}
+
+condLoop();
+
+function diretoNoElemento(){
+    console.log('olá mundo!');
+}
