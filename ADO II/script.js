2ef3cfe826048ba2394729665dfc1275252c726c
Rafael M <99418707+Mr-R4F@users.noreply.github.com>
2022-06-01 17:29:01 -0300

    ADO II - API números

---
+++ ADO II/script.js
@@ -0,0 +1,31 @@
+document.getElementById('nome').focus();
+
+document.getElementById('btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const nome = document.getElementById('nome').value;
+
+    if (nome === '') {
+        document.getElementById('aviso').removeAttribute('hidden');
+        document.getElementById('aviso').innerText = 'Preencha o campo';
+        document.getElementById('nome').focus();
+    } else {
+        document.getElementById('aviso').setAttribute('hidden', 'on');
+
+        fetch(`https://api.agify.io?name=${nome}`).then(function(resultado) {
+        return resultado.json();
+        }).then(function(conteudo) {
+            document.getElementById('resultado').innerText = "Sua idade é: " + conteudo.age;
+            document.getElementById('nome').focus();
+        });
+    }
+});
+
+
+$(document).ready(function() {
+    $("h1").hover(function() {
+        $(this).css('font-size', 'large');
+    }, function() {
+        $(this).css('font-size', 'medium');
+    }); 
+});
+
